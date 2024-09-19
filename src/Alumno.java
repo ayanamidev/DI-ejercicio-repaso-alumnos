2e615e179b5fee43d80ee35f05f25ca4e57b8122
LauraDev <lauralodeirodev@gmail.com>
2024-09-19 12:08:20 +0200

    Primer commit

---
+++ src/Alumno.java
@@ -0,0 +1,15 @@
+
+public class Alumno {
+	String nombre;
+	int edad;
+	String ciudad;
+	Double matematicas;
+	Double ingles;
+	Double programacion;
+	
+	public Double media(double matematicas,double ingles,double programacion) {
+		double 
+		return resultado;
+	}
+
+}
